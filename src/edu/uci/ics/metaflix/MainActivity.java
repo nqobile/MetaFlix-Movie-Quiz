905451e148b5495e16204df9608c79475b386229
nqobile <istephen@uci.edu>
2014-03-12 03:45:43 -0700

    -Added code that launches into QuizActivity upon pressing "Start Quiz" button
    -Created the QuizHandler in onCreate method

--- src/edu/uci/ics/metaflix/MainActivity.java
+++ src/edu/uci/ics/metaflix/MainActivity.java
@@ -1,22 +1,35 @@
 package edu.uci.ics.metaflix;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		// Create the quiz handler 
+		QuizHandler qh = new QuizHandler();
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	public void startQuizHandler(View view)
+	{
+		// Launch quiz activity
+		startActivity(new Intent(this, QuizActivity.class));
 	}
 
 }
