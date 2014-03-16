b7de8f12bb50f828e221e27928a483b4ed9f584e
nqobile <istephen@uci.edu>
2014-03-15 21:35:46 -0700

    Test post plz ignore

--- gen/edu/uci/ics/metaflix/R.java
+++ gen/edu/uci/ics/metaflix/R.java
@@ -24,26 +24,38 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000b;
+        public static final int action_settings=0x7f080015;
         public static final int answerFour=0x7f080008;
         public static final int answerOne=0x7f080005;
         public static final int answerRadioButtonGroup=0x7f080004;
         public static final int answerThree=0x7f080007;
         public static final int answerTwo=0x7f080006;
+        public static final int averageTimePerQuestionLabel=0x7f080013;
+        public static final int averageTimePerQuestionValue=0x7f080014;
+        public static final int overallScoreLabel=0x7f08000b;
+        public static final int overallScoreValue=0x7f08000c;
         public static final int questionText=0x7f080003;
         public static final int quizButton=0x7f080001;
         public static final int statsButton=0x7f080002;
         public static final int submitAnswerButton=0x7f080009;
         public static final int textView1=0x7f080000;
         public static final int timeRemaining=0x7f08000a;
+        public static final int totalCorrectAnswersLabel=0x7f080010;
+        public static final int totalCorrectAnswersValue=0x7f08000f;
+        public static final int totalQuizzesTakenLabel=0x7f08000d;
+        public static final int totalQuizzesTakenValue=0x7f08000e;
+        public static final int totalWrongAnswersLabel=0x7f080011;
+        public static final int totalWrongAnswersValue=0x7f080012;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_quiz=0x7f030001;
+        public static final int activity_statistics=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
         public static final int quiz=0x7f070001;
+        public static final int statistics=0x7f070002;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -55,6 +67,13 @@ public final class R {
         /**  MainActivity 
          */
         public static final int app_name=0x7f050000;
+        public static final int averageTimePerQuestionLabel=0x7f050017;
+        public static final int averageTimePerQuestionValue=0x7f050018;
+        public static final int hello_world=0x7f05000e;
+        public static final int overallScoreLabel=0x7f050010;
+        /**  StatisticsActivity 
+         */
+        public static final int overallScoreValue=0x7f05000f;
         public static final int question=0x7f050006;
         public static final int quizStartButtonLabel=0x7f050003;
         public static final int quizStatisticsButtonLabel=0x7f050004;
@@ -62,6 +81,13 @@ public final class R {
         /**  QuizActivity 
          */
         public static final int title_activity_quiz=0x7f050005;
+        public static final int title_activity_statistics=0x7f05000d;
+        public static final int totalCorrectAnswersLabel=0x7f050013;
+        public static final int totalCorrectAnswersValue=0x7f050014;
+        public static final int totalQuizzesTakenLabel=0x7f050011;
+        public static final int totalQuizzesTakenValue=0x7f050012;
+        public static final int totalWrongAnswersLabel=0x7f050015;
+        public static final int totalWrongAnswersValue=0x7f050016;
         public static final int welcome=0x7f050002;
     }
     public static final class style {
