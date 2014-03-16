fafee02250d4ff753843274adfcda68b5a49a289
nqobile <istephen@uci.edu>
2014-03-15 21:34:43 -0700

    Fixed gitignore

--- src/edu/uci/ics/metaflix/MainActivity.java
+++ src/edu/uci/ics/metaflix/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends Activity {
 		 * Create the databases and populate it
 		 */
 		DbAdapter dba = new DbAdapter(this);
+		
 	}
 
 	@Override
--- src/edu/uci/ics/metaflix/StatisticsActivity.java
+++ src/edu/uci/ics/metaflix/StatisticsActivity.java
@@ -13,7 +13,8 @@ public class StatisticsActivity extends Activity {
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.statistics, menu);
 		return true;
