3b90e3c6b6bd224846ad60793a09a3856a5f5e00
nqobile <istephen@uci.edu>
2014-03-15 22:43:32 -0700

    Finished Question.java

--- src/edu/uci/ics/metaflix/Question.java
+++ src/edu/uci/ics/metaflix/Question.java
@@ -2,6 +2,7 @@ package edu.uci.ics.metaflix;
 
 public class Question
 {
+	// Fields
 	private String question;
 	private int correctAnswerNumber;
 	private String answerZero;
@@ -9,9 +10,49 @@ public class Question
 	private String answerTwo;
 	private String answerThree;
 	
-	public Question(int questionType)
+	
+	
+	// Constructor
+	public Question(String question, 
+			int correctAnswerNumber, 
+			String answerZero, 
+			String answerOne, 
+			String answerTwo, 
+			String answerThree)
 	{
+		this.question = question;
+		this.correctAnswerNumber = correctAnswerNumber;
+		this.answerZero = answerZero;
+		this.answerOne = answerOne;
+		this.answerTwo = answerTwo;
+		this.answerThree = answerThree;
 	}
 	
-	public String 
+	
+	
+	// Methods
+	public String getQuestion()
+	{
+		return question;
+	}
+	public int getCorrectAnswerNumber()
+	{
+		return correctAnswerNumber;
+	}
+	public String getAnswerZero()
+	{
+		return answerZero;
+	}
+	public String getAnswerOne()
+	{
+		return answerOne;
+	}
+	public String getAnswerTwo()
+	{
+		return answerTwo;
+	}
+	public String getAnswerThree()
+	{
+		return answerThree;
+	}
 }
