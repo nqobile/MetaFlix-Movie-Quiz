b89a81100b311356738253f29fd62b9d6cb6d2be
Mason <nosammai@gmail.com>
2014-03-16 17:50:31 -0700

    Implemented More questions
    still need testing

--- src/edu/uci/ics/metaflix/QuestionAdapter.java
+++ src/edu/uci/ics/metaflix/QuestionAdapter.java
@@ -158,6 +158,7 @@ public class QuestionAdapter {
 	    		
 	    		query2 = "SELECT first_name, last_name FROM stars, stars_in_movies WHERE (stars.id = stars_in_movies.star_id) AND movie_id = " + results.getInt(0) + " ORDER BY RANDOM() LIMIT 2";
 	    		results2 = this.mDb.rawQuery(query2, null);
+	    		results2.moveToFirst();
 	    		
 	    		correctAnswerIndex = randomGen.nextInt(4);
 	    		answers.add(correctAnswerIndex, results.getString(1));
@@ -189,8 +190,78 @@ public class QuestionAdapter {
 	    		return new Question("Who directed the star " + results.getString(1) + " " + results.getString(2) + "?", correctAnswerIndex, answers);
 	    		
 	    	case 5:
+	    		answers = new ArrayList<String>();
+	    		query = "SELECT sm.star_id, s.first_name, s.last_name FROM stars_in_movies sm, stars s WHERE sm.star_id = s.id GROUP BY star_id HAVING count(movie_id) > 1 ORDER BY RANDOM() LIMIT 1";
+	    		results = this.mDb.rawQuery(query, null);
+	    		results.moveToFirst();
+	    		
+	    		query2 = "SELECT m.title FROM stars_in_movies sm, movies m WHERE sm.movie_id = m.id AND sm.star_id=" + results.getInt(0)+ " ORDER BY RANDOM() LIMIT 2";
+	    		results2 = this.mDb.rawQuery(query2, null);
+	    		results2.moveToFirst();
+	    		
+	    		wrongAnsQuery = "SELECT first_name, last_name FROM stars ORDER BY RANDOM() LIMIT 3";
+	    		wrongResults = this.mDb.rawQuery(wrongAnsQuery,null);
+	    		
+	    		while(wrongResults.moveToNext())
+	    		{
+	    			answers.add(wrongResults.getString(0) + " " + wrongResults.getString(1));
+	    		}
+	    		correctAnswerIndex = randomGen.nextInt(4);
+	    		answers.add(correctAnswerIndex, results.getString(1) + " " + results.getString(2));
+	    		question = "Which star appears in both movies " + results2.getString(0) + " and ";
+	    		results2.moveToNext();
+	    		question += results2.getString(0) + "?";
+	    		return new Question(question, correctAnswerIndex, answers);
+	    		
 	    	case 6:
+	    		answers = new ArrayList<String>();
+	    		query = "SELECT movies.id FROM stars, stars_in_movies, movies WHERE (stars.id = stars_in_movies.star_id AND movies.id = stars_in_movies.movie_id) GROUP BY movies.id HAVING count(stars.id)>3 ORDER BY RANDOM() LIMIT 1";
+	    		results = this.mDb.rawQuery(query, null);
+	    		results.moveToFirst();
+	    		
+	    		//First answer is the question person, next 3 are wrong answers.
+	    		query2 = "SELECT s.first_name, s.last_name FROM stars_in_movies sm, stars s WHERE sm.star_id = s.id AND sm.movie_id=" + results.getInt(0);
+	    		results2 = this.mDb.rawQuery(query2, null);
+	    		
+	    		   		
+	    		for(int i = 0; i < 3; i++)
+	    		{
+	    			results2.moveToNext();
+	    			answers.add(results2.getString(0) + " " + results2.getString(1));
+	    		}
+	    		
+	    		//This one is actually right.
+	    		wrongAnsQuery = "SELECT first_name, last_name FROM stars ORDER BY RANDOM() LIMIT 1";
+	    		wrongResults = this.mDb.rawQuery(wrongAnsQuery, null);	 
+	    		wrongResults.moveToFirst();
+	    		
+	    		correctAnswerIndex = randomGen.nextInt(4);
+	    		results2.moveToNext();
+	    		answers.add(correctAnswerIndex, results2.getString(0) + " " + results2.getString(1));
+	    		question = "Which star did not appear in the same movie with the star " + wrongResults.getString(0) + " " + wrongResults.getString(1) + "?";
+	    		return new Question(question, correctAnswerIndex, answers);
 	    	case 7:
+	    		answers = new ArrayList<String>();
+	    		query = "SELECT s.id, s.first_name, s.last_name FROM stars s, stars_in_movies sm WHERE s.id=sm.star_id ORDER BY RANDOM() LIMIT 1";
+	    		results = this.mDb.rawQuery(query, null);
+	    		results.moveToFirst();
+	    		
+	    		query2 = "SELECT m.director, m.year FROM stars s, movies m, stars_in_movies sm WHERE s.id=sm.star_id AND m.id=sm.movie_id" +
+	    				" AND s.id=" + results.getInt(0) + " ORDER BY RANDOM() LIMIT 1";
+	    		results2 = this.mDb.rawQuery(query2, null);
+	    		results2.moveToFirst();	    		
+	    		
+	    		wrongAnsQuery = "SELECT director FROM movies ORDER BY RANDOM() LIMIT 3";
+	    		wrongResults = this.mDb.rawQuery(wrongAnsQuery, null);
+	    		
+	    		while(wrongResults.moveToNext())
+	    		{
+	    			answers.add(wrongResults.getString(0));
+	    		}
+	    		correctAnswerIndex = randomGen.nextInt(4);
+	    		answers.add(correctAnswerIndex, results2.getString(0));
+	    		return new Question("Who directed the star " + results.getString(1) + " " + results.getString(2) + " in the year " + results2.getInt(1) + "?", correctAnswerIndex, answers);
+	    		
 	    	case 8:
 	    	default:
 	    		return null;
